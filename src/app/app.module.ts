eff946ee1712378c0d9f9e32f517a54d3c0bb5b0
Hugo Silva <hugoengcomp@gmail.com>
2020-03-30 08:42:32 -0300

    Exemplo de um fab menu usando o angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpeedDialFabComponent } from './speed-dial-fab/speed-dial-fab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeedDialFabComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
